9ea1ea60fd75a28778d4e42ead14a63a17d206c2
Konstantin <skdin85@gmail.com>
2017-09-07 14:07:41 +0300

    Create static pages controller and english version for pm intervit group

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def catalog
+  end
+
+  def about
+  end
+
+  def home_en
+  end
+
+  def catalog
+  end
+
+  def about_en
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/home_en'
+
+  get 'static_pages/catalog'
+
+  get 'static_pages/about_en'
+
   root 'application#hello'
 end
